d6079f7a538bb25823d501408162317b68d3565e
Pongsakorn Sommalai <bongtrop@gmail.com>
2016-03-25 03:45:12 +0700

    fix dtype

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ class IdentiFace(Resource):
                 lk = len(keypoints)
 
                 for data in datas:
-                    if data["dype"]!="strgramma":
+                    if data["dtype"]!="strgramma":
                         data["point"] = tool.match(keypoints, data["keypoints"])*1.0/lk
                     else:
                         data["point"] = 0.0
